92e2e5623dd250f1c17dbf6a8e94488dbd62f592
kamilsci <sunnysideprd@gmail.com>
2024-07-20 13:58:50 -0700

    new test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,51 +1,50 @@
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
+
 import AppFunctional from './AppFunctional'
 
-test('renders a 3x3 grid', () => {
-  render(<AppFunctional />)
+describe('AppFunctional', () => {
+  let wrapper
 
-  const squares = screen.getAllByText(/B/)
-  expect(squares.length).toBe(9)
-})
+  beforeEach(() => {
+    wrapper = render(<AppFunctional />)
+  })
 
-test('when moving left, the B moves left', () => {
-  render(<AppFunctional />)
+  test('renders a 3x3 grid', () => {
+    const squares = wrapper.queryAllByText(/B/)
+    expect(squares.length).toBe(9)
+  })
 
-  const leftButton = screen.getByText('Left')
-  fireEvent.click(leftButton)
+  test('when moving left, the B moves left', () => {
+    const leftButton = wrapper.getByText('Left')
+    fireEvent.click(leftButton)
 
-  const square = screen.getByText(/B/)
-  expect(square).toHaveAttribute('data-testid', 'square-1')
-})
+    const square = wrapper.getByText(/B/)
+    expect(square).toHaveAttribute('data-testid', 'square-1')
+  })
 
-test('when moving up, the B moves up', () => {
-  render(<AppFunctional />)
+  test('when moving up, the B moves up', () => {
+    const upButton = wrapper.getByText('Up')
+    fireEvent.click(upButton)
 
-  const upButton = screen.getByText('Up')
-  fireEvent.click(upButton)
+    const square = wrapper.getByText(/B/)
+    expect(square).toHaveAttribute('data-testid', 'square-3')
+  })
 
-  const square = screen.getByText(/B/)
-  expect(square).toHaveAttribute('data-testid', 'square-3')
-})
+  test('when moving right, the B moves right', () => {
+    const rightButton = wrapper.getByText('Right')
+    fireEvent.click(rightButton)
 
-test('when moving right, the B moves right', () => {
-  render(<AppFunctional />)
+    const square = wrapper.getByText(/B/)
+    expect(square).toHaveAttribute('data-testid', 'square-5')
+  })
 
-  const rightButton = screen.getByText('Right')
-  fireEvent.click(rightButton)
+  test('when moving down, the B moves down', () => {
+    const downButton = wrapper.getByText('Down')
+    fireEvent.click(downButton)
 
-  const square = screen.getByText(/B/)
-  expect(square).toHaveAttribute('data-testid', 'square-5')
-})
-
-test('when moving down, the B moves down', () => {
-  render(<AppFunctional />)
-
-  const downButton = screen.getByText('Down')
-  fireEvent.click(downButton)
-
-  const square = screen.getByText(/B/)
-  expect(square).toHaveAttribute('data-testid', 'square-7')
+    const square = wrapper.getByText(/B/)
+    expect(square).toHaveAttribute('data-testid', 'square-7')
+  })
 })
 
